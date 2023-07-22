708bcdc12de21d23fa6076ecc17ed54a34ff4f5f
draiguv <dcrauv13@gmail.com>
2023-07-23 00:44:17 +0800

    Refactor App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,21 @@ import React from 'react'
 import Card from './Card.jsx'
 import contacts from '../data/contacts.js'
 
-const createCard = contact => {
-  return (
-    <Card 
-      key={contact.id}
-      id={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
-    />
-  )
-}
-
 export default function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-        {contacts.map(createCard)}
+        {contacts.map(contact => 
+            <Card 
+              key={contact.id}
+              id={contact.id}
+              name={contact.name}
+              img={contact.imgURL}
+              tel={contact.phone}
+              email={contact.email}
+            />
+          )
+        }
     </div>
   )
 }
